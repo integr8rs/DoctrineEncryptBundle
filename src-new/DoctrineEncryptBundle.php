7b537ca4cacdaea3d04f750bcd2b57598d4f578c
Zombaya <Zombaya@users.noreply.github.com>
2024-12-16 20:31:59 +0100

    Small cleanup

--- src-new/DoctrineEncryptBundle.php
+++ src-new/DoctrineEncryptBundle.php
@@ -2,18 +2,8 @@
 
 namespace DoctrineEncryptBundle\DoctrineEncryptBundle;
 
-use Ambta\DoctrineEncryptBundle\DependencyInjection\VersionTester;
-use DoctrineEncryptBundle\DoctrineEncryptBundle\DependencyInjection\ServiceConfigurator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class DoctrineEncryptBundle extends Bundle
 {
-    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
-    {
-        $serviceConfigurator = new ServiceConfigurator(new VersionTester(), true);
-
-        $serviceConfigurator->configure($config, $builder);
-    }
 }
