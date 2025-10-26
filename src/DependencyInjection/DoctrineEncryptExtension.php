06393dba911797fb867df31ad5183e6efe6e89e9
Piet Bosch <pjcbosch@gmail.com>
2025-10-26 13:13:58 +0200

    Fix deprecation notice to use Symfony\Component\DependencyInjection\Extension\Extension instead of Symfony\Component\HttpKernel\DependencyInjection\Extension (#64)

--- src/DependencyInjection/DoctrineEncryptExtension.php
+++ src/DependencyInjection/DoctrineEncryptExtension.php
@@ -6,8 +6,8 @@ use Ambta\DoctrineEncryptBundle\Encryptors\DefuseEncryptor;
 use Ambta\DoctrineEncryptBundle\Encryptors\HaliteEncryptor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\HttpKernel\Kernel;
 
 /**
